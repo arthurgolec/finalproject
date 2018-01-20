23c04be0f1b9d6b6c6dc5036e1c9f917a2bee280
arthurgolec <agolec@ucvts.tec.nj.us>
2018-01-20 03:26:00 +0000

    Added a dodge function in the making, made a timer system for champ select, and made teammate class

--- leagueoflegends.py
+++ leagueoflegends.py
@@ -1,10 +1,23 @@
 import random
 import time
+from threading import Timer
 #lets us make the game random
 
 lanes = ["top", "mid", "jungle", "bot", "support"]
-usernames = ["t1", "solofeed", "aniviabot", "urmomhahaha", "CodySimpsonFan", "jgorfeed", "epicwin", "hashinshin", "dardoch"]
 
+class Teammate:
+    def __init__(self, username, champion, lane):
+        self.username = username
+        self.champion = champion
+        self.lane = lane
+
+player1 = Teammate("sorakamain1", "soraka", "support")
+player2 = Teammate("tyler1", "draven", "bot")
+player3 = Teammate("shiptur", "azir", "mid")
+player4 = Teammate("peanut", "sejuani", "jungle")
+player5 = Teammate("hashinshin", "jax", "top")
+
+pickablechampions = ("teemo", "riven", "sejuani", "kha'zix", "azir", "leblanc", "draven", "tahm kench", "alistar")
 
 class Player(object):
     """The player's stats throughout the game"""
@@ -21,6 +34,34 @@ class Player(object):
         self.ultimatemana = ultimatemana
         self.ability1skillshot = ability1skillshot
         self.ultimateskillshot = ultimateskillshot
+
+def dodge():
+    """This function allows us to dodge several times without having to do this alot"""
+    print("Welcome back to the game? Would you like to play?")
+    print("Of course you do, lets find you a game")
+    print("before that, we have to find you another lane")
+    laneselect1 = input("So what lane do you want to play?  ")
+    while True:
+        if laneselect1 == "lanes":
+            print('"top", "mid", "jungle", "bot", "support"')
+            laneselect1 = input("So what lane do you want to play?  ")
+        elif laneselect1 in lanes:
+            print("Got it, searching for game")
+            lanes.remove(laneselect1)
+            time.sleep(1)
+            print("...")
+            time.sleep(1)
+            print("...")
+            break
+        else:
+            laneselect1 = input("I didn't get that. What lane do you want to play?  ")
+        for x in range(20):
+            print(f"{x} minutes have passed. {20-x} minutes remaining")
+            for x in range(4): #this will show a ... every 15 seconds of waiting
+                time.sleep(15)
+                print("...")
+        #this is going to show the 20 minute ban for leaving the game
+
 
 
 pusername = input("Hey! Before we get started, what is your username?  ")
@@ -59,29 +100,26 @@ print(f"Ah, narts, you got autofilled to {playeractuallane}. Unfortunate!")
 print("loading team chat.....")
 time.sleep(5)
 print("""
-
-
-
-
-
-
-
 _____________________________________________________
 """)
 #makes a space showing chat log prompt
 
-print(f"-->{random.choice(usernames)} ~ hey guys! glhf")
+time.sleep(2)
+print(f"-->{player1.username} ~ hey guys! glhf")
 
-time.sleep(.5)
-print(f"""-->{random.choice(usernames)} ~ {playeractuallane} or i feed
-
+time.sleep(1.5)
+print(f"""-->{player2.username} ~ {player2.lane} or i feed
 """)
 
-game_dodge_decision = input("Uh oh, a player is planning on inting. Do you want to leave and find another match or stay and wait it out?")
+print("""
+_____________________________________________________
+""")
+
+game_dodge_decision = input("Uh oh, a player is planning on inting. Do you want to leave and find another match or stay and wait it out?  ")
 while True:
-    if game_dodge_decision == "leave" or "leave and find another match" or "find another match":
-        leave_game_decision = input("Are you sure? You already left a few matches, so leaving now would result in a 20 minute ban.").lower()
-        if leave_game_decision == "yes" or "y":
+    if game_dodge_decision == "leave" or game_dodge_decision == "leave and find another match" or game_dodge_decision == "find another match":
+        leave_game_decision = input("Are you sure? You already left a few matches, so leaving now would result in a 20 minute ban.  ").lower()
+        if leave_game_decision == "yes" or leave_game_decision == "y":
             print("Ok, leaving match")
             time.sleep(2)
             print("Welcome back to the game? Would you like to play?")
@@ -108,17 +146,69 @@ while True:
                         time.sleep(15)
                         print("...")
                 #this is going to show the 20 minute ban for leaving the game
-        if leave_game_decision == "no" or "n":
+        if leave_game_decision == "no" or leave_game_decision == game_dodge_decision == "n":
             print("Ok, good call")
             break
             #now the player goes back to the main game
         else:
-            leave_game_decision = input("Sorry, didn't get that. Type 'yes' to leave or 'no' to stay. ")
+            leave_game_decision = input("Sorry, didn't get that. Type 'yes' to leave or 'no' to stay.  ")
     if game_dodge_decision == "stay" or "stay and wait it out" or "wait it out":
         print("Good call, carry on")
         break
     else:
         game_dodge_decision = input("Sorry, i didn't get that. Type 'leave' to leave or 'stay' to stay")
+
+time.sleep(2)
+
+print("""
+_____________________________________________________
+""")
+
+print(f"-->{player3.username} ~ {player2.username} plz kys")
+time.sleep(2.3)
+print(f"-->{player1.username} ~ fine just give it to him")
+time.sleep(1.4)
+print("""
+_____________________________________________________
+""")
+playertext1 = input("Say something mean to this guy!  ")
+print("""
+_____________________________________________________
+""")
+print(f"-->{pusername} ~ {playertext1}")
+time.sleep(2)
+print(f"-->{player2.username} ~ if someone says one more thing i run it down mid")
+print("""
+_____________________________________________________
+""")
+print("oh, narts, you were talking too much and forgot to pick a champion!")
+time.sleep(2)
+print("""the champions that you can choose are:
+Teemo
+Riven
+Sejuani
+Kha'Zix
+Azir
+LeBlanc
+Draven
+Xayah
+Tahm Kench
+Alistar
+""")
+time.sleep(5)
+
+timeout = 7.5
+t = Timer(timeout, print, ['Sorry, times up. You dodged'])
+t.start()
+prompt = "You have %d seconds to pick a champion...\n" % timeout
+championselected = input(prompt).lower()
+if championselected not in pickablechampions:
+    print("DIDN'T GET THAT GO QUICK")
+    championselected = input(prompt).lower()
+t.cancel()
+print("phew, that was close!")
+if t == 0:
+    print("wow, time to search for a new match")
 
 
 
